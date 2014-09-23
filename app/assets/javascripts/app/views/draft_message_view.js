99e3012f0240bc6c92e3525a7edcdf29d3114f6e
Daniel Sun <dan.mi.sun@gmail.com>
2014-09-23 07:28:58 +0100

    refactor draftable code

--- app/assets/javascripts/app/views/draft_message_view.js
+++ app/assets/javascripts/app/views/draft_message_view.js
@@ -9,13 +9,10 @@ app.views.DraftMessageView = app.views.Base.extend({
   },
 
   initialize: function(options) {
-    //each time obj instantiated it is being overwritten
-    //need to figure out how to dynamically assing id
-    //need logic to check if draft already exists -> possibly refactor save
-    this.model = new app.models.MessageDraft({ id : 1 });
-    //is it possible to DRY up # of saves by watching for changes?
-    //this.listenTo(this, 'change', this.save);
-    this.model.save();
+    //set message id to url query string
+    //
+    this.model = new app.models.MessageDraft({ id : window.location.toString() });
+    this.model.fetch();
   },
 
   render: function() {
@@ -26,16 +23,14 @@ app.views.DraftMessageView = app.views.Base.extend({
 
   setTo: function(e) {
    this.model.set("to", e.currentTarget.value);
-   this.model.save();
   },
 
   setSubject: function(e) {
    this.model.set("subject", e.currentTarget.value);
-   this.model.save();
   },
 
   setText: function(e) {
    this.model.set("text", e.currentTarget.value);
-   this.model.save();
   }
+
 });
