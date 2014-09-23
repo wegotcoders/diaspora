b5616318116c676a8a43ea342af69e498668d774
Daniel Sun <dan.mi.sun@gmail.com>
2014-09-23 07:28:58 +0100

    refactor draftable code

--- app/assets/javascripts/app/models/message_draft.js
+++ app/assets/javascripts/app/models/message_draft.js
@@ -1,5 +1,11 @@
 app.models.MessageDraft = Backbone.Model.extend({
 
-  localStorage : new Backbone.LocalStorage("MessageDraft"),
+  localStorage : new Backbone.LocalStorage("MessageDraft"), 
+
+  initialize: function(options) {
+    //make this message draftable and add timer function for localStorage
+    //
+    Backbone.makeDraftable(this);    
+  }
 
 });
