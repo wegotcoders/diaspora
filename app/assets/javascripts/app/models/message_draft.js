c1bc5318d96e5b73a4051dea10195a577929e66b
Daniel Sun <dan.mi.sun@gmail.com>
2014-09-30 17:03:44 +0100

    wip destroy draft in local storage

--- app/assets/javascripts/app/models/message_draft.js
+++ app/assets/javascripts/app/models/message_draft.js
@@ -2,10 +2,16 @@ app.models.MessageDraft = Backbone.Model.extend({
 
   localStorage : new Backbone.LocalStorage("MessageDraft"), 
 
+
   initialize: function(options) {
     //make this message draftable and add timer function for localStorage
     //
     Backbone.makeDraftable(this);    
+    this.listenTo(this, 'sync', this.removeLocalStorage);
+  },
+
+  removeLocalStorage : function() {
+    this.destroy();
   }
 
 });
